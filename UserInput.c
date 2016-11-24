e1a4a81b7bc842918db52b4f5344b0917e12bd56
Alameer Ashraf Hassan <alamiir.ashraf@gmail.com>
2016-11-24 14:14:54 +0200

    NewUserInput
    Chang have been made to satidfy the Borland C compliler , if you use
    Code Blocks the old one will run correctly .

--- UserInput.c
+++ UserInput.c
@@ -1,13 +1,30 @@
-void UserInput (char Value[])
+void UseerInput(char Value[])
 {
-   int i = 0 ;
-   char c ;
-   c=getchar();
-   do
-   {
-       Value[i++]=c;
-       c=getchar();   //i+1
-   }
-   while(c != '\n');
+    int i = 0, DetectingFlag = 1;
+    int ch;
 
+    while(flag)
+    {
+		ch = getchar();
+        //Extended Key entered !
+		if(ch == 0)
+		{
+		 ch = getchar();
+		}
+        //Backspace entered !! 
+		else if(ch == '8')
+		{
+            printf("\b \b");
+			i--;
+		}
+		else if(isprint(ch))
+		{
+			Value[i++] = ch;
+		}
+		else if(ch == 13)
+		{
+			DetectingFlag = 0;
+		}
+    }
+    Value[i] = 0;
 }
