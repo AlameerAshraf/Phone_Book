b2abbe38b58dd894caaa91df12e54f4b9f1a6e4f
Alameer Ashraf Hassan <alamiir.ashraf@gmail.com>
2016-11-29 00:01:02 +0200

    User Input
    Take the user's data without using scanf !

--- UserInput.c
+++ UserInput.c
@@ -1,32 +1,14 @@
-void Userinput(char name[])
+/* User input to take input from user */
+void UserInput (char Value[])
 {
-    int i = 0, flag = 1;
-    int ch;
+   int i = 0 ;
+   char c ;
+   c=getchar();
+   do
+   {
+       Value[i++]=c;
+       c=getchar();   //i+1
+   }
+   while(c != '\n');
 
-    while(flag)
-    {
-		ch = getch();
-		if(ch == 0)
-		{
-		 ch = getch();
-		}
-		else if(ch == '8')
-		{
-            printf("\b");
-			printf(" ");
-			printf("\b");
-			i--;
-		}
-		else if(isprint(ch))
-		{
-			name[i] = ch;
-			printf("%c",name[i]);
-			i++;
-		}
-		else if(ch == 13)
-		{
-			flag = 0;
-		}
-    }
-    name[i] = 0;
 }
