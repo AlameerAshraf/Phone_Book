2d1fec401d9c31db7a0405c33119d41d762f8da0
Alameer Ashraf Hassan <alamiir.ashraf@gmail.com>
2016-11-29 00:10:49 +0200

    Add node function
    add new node to the list !

---
+++ AddNode.c
@@ -0,0 +1,21 @@
+// add a new node in the linked list 
+int AddNode()
+{
+    struct Record* Pointer ;
+    int AdditionFlag = 0 ; 
+    if (Head == NULL && Tile == NULL) //Tile is the last node in the linked list ! 
+    {
+        Head = Tile = CreateNode(); // create node function to be called here ! 
+        AdditionFlag = 1 ;  // set the flag to one !
+    }
+    else
+    {
+        Pointer = CreateNode();
+        Tile -> Next = Pointer ;   // next of last node refering to the new node ! 
+        Pointer -> Prev = Tile ;   // prevous of the new node refer to the last node (tile) !
+        Pointer -> Next = NULL;    // next of the new will be null ! 
+        Tile = Pointer ;           // the new node will be the tile ! 
+    }
+
+    return AdditionFlag ; 
+}
